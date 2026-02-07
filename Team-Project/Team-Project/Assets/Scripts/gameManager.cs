2778c8077b3c168d9650ad73b624a3e96bf7eaa6
Miguel Flores <miguelflores2691@gmail.com>
2026-02-07 13:37:22 -0500

    gameManager set up

---
+++ Team-Project/Team-Project/Assets/Scripts/gameManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    public static gameManager instance;
+
+    
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject player;
+    //TODO playerScript Pending Miyu's script
+
+
+    //TODO HP bar + Damage Flash Matt
+
+
+    public bool isPaused;
+    float timeScaleOrig;
+
+    //will be set to true in another script when player enters collider for the goal Miguel
+    public bool gameGoalReached;
+    GameObject goalObject;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+
+        gameGoalReached = false;
+        timeScaleOrig = Time.timeScale;
+
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO pause menu code Garrett
+    }
+
+    public void updateGameGoal()
+    {
+        if (gameGoalReached)
+        {
+            youWin();
+        }
+    }
+    
+    public void youWin()
+    {
+        //you win menu code Garrett
+
+    }
+
+    public void youLose()
+    {
+        //you lose menu code Garrett
+
+    }
+}
