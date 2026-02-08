283c51d340b55cbb05cacb43c98002aa7bbc3088
Velma187 <143227625+Velma187@users.noreply.github.com>
2026-02-08 04:15:04 -0500

    created crouch function and camera movement with crouch

--- Team-Project/Team-Project/Assets/Scripts/playerControlller.cs
+++ Team-Project/Team-Project/Assets/Scripts/playerControlller.cs
@@ -7,10 +7,16 @@ public class playerControlller : MonoBehaviour
     [SerializeField] int HP;
     [SerializeField] int speed;
     [SerializeField] int sprintMod;
-    [SerializeField] int crouchMod;
     [SerializeField] int jumpSpeed;
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
+
+    [SerializeField] int crouchMod;
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float crouchCameraOffset;
+    [SerializeField] float crouchLerpSpeed;
+    [SerializeField] float crouchHeight;
+    [SerializeField] float standHeight;
 
     int jumpCount;
     int HPOrig;
@@ -20,13 +26,17 @@ public class playerControlller : MonoBehaviour
 
     Vector3 moveDir;
     Vector3 playerVeloc;
-    
-    
-    
+    Vector3 playerCenterOrig;
+    Vector3 cameraStartPos;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cameraStartPos = playerCamera.localPosition;
+        standHeight = controller.height;
+        playerCenterOrig = controller.center;
     }
 
     // Update is called once per frame
@@ -35,21 +45,22 @@ public class playerControlller : MonoBehaviour
         movement();
         sprint();
         crouch();
+        crouchVisual();
     }
 
     void movement()
     {
-        if(controller.isGrounded)
+        if (controller.isGrounded)
         {
             jumpCount = 0;
             playerVeloc = Vector3.zero;
         }
 
-        moveDir =  Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         controller.Move(moveDir * speed * Time.deltaTime);
 
         jump();
-                
+
         controller.Move(playerVeloc * Time.deltaTime);
         playerVeloc.y -= gravity * Time.deltaTime;
 
@@ -57,7 +68,7 @@ public class playerControlller : MonoBehaviour
 
     void jump()
     {
-        if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
         {
             playerVeloc.y = jumpSpeed;
             jumpCount++;
@@ -67,11 +78,11 @@ public class playerControlller : MonoBehaviour
 
     void sprint()
     {
-        if(Input.GetButtonDown("Sprint"))
+        if (Input.GetButtonDown("Sprint"))
         {
             speed *= sprintMod;
         }
-        else if(Input.GetButtonUp("Sprint"))
+        else if (Input.GetButtonUp("Sprint"))
         {
             speed /= sprintMod;
         }
@@ -79,18 +90,55 @@ public class playerControlller : MonoBehaviour
 
     void crouch()
     {
-        if (Input.GetButtonDown("Crouch"))
-        {
-            isCrouching = !isCrouching;
+        if (!Input.GetButtonDown("Crouch"))
+            return;
 
-            if (isCrouching)
-            {
-                speed /= crouchMod;
-            }
-            else
-            {
-                speed *= crouchMod;
-            }
+        bool wantToCrouch = !isCrouching;
+
+        if (!wantToCrouch)
+        {
+            Vector3 rayStart = transform.position + Vector3.up * controller.height;
+            float rayDistance = standHeight - controller.height;
+
+            if (Physics.Raycast(rayStart, Vector3.up, rayDistance))
+                return;
+        }
+
+        isCrouching = wantToCrouch;
+
+        if (isCrouching)
+        {
+            speed /= crouchMod;
+            controller.height = crouchHeight;
+            controller.center = new Vector3(
+                controller.center.x,
+                crouchHeight / 2f,
+                controller.center.z
+            );
+        }
+        else
+        {
+            speed *= crouchMod;
+            controller.height = standHeight;
+            controller.center = playerCenterOrig;
         }
     }
+
+
+    void crouchVisual()
+    {
+        Vector3 targetPos = cameraStartPos;
+
+        if (isCrouching)
+        {
+            targetPos.y -= crouchCameraOffset;
+        }
+
+        playerCamera.localPosition = Vector3.Lerp(
+            playerCamera.localPosition,
+            targetPos,
+            crouchLerpSpeed * Time.deltaTime
+        );
+    }
+
 }
